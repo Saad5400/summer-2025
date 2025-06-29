a00d3158903fba35ea26b5b5edbb6cb256f02fd6
Saad5400 <sdbtwa@gmail.com>
2025-06-29 21:51:55 +0300

    forms - login page

--- resources/views/login/create.blade.php
+++ resources/views/login/create.blade.php
@@ -1,12 +1,12 @@
 <x-layouts.auth>
-    <div class="space-y-2">
+    <form class="space-y-2">
         <x-input id="email" label="البريد الالكتروني" icon="icon-[tabler--mail]" type="email" />
         <x-input id="password" label="كلمة المرور" icon="icon-[tabler--lock]" type="password" />
 
-        <button class="btn btn-primary w-full mt-8">تسجيل دخول</button>
+        <button type="submit" class="btn btn-primary w-full mt-8">تسجيل دخول</button>
         <span>
             ما عندك حساب؟
             <a href="/register" class="link link-animated">سجل حساب</a>
         </span>
-    </div>
+    </form>
 </x-layouts.auth>
